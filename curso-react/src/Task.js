41fc6916df7ea59a836681ac4988c30f38c58b40
Alex <alrodriguez41@poligran.edu.co>
2022-11-09 17:36:13 -0500

    Estilos en react

---
+++ curso-react/src/Task.js
@@ -0,0 +1,7 @@
+export function TaskCard(){
+    return <div style={{background:"#000000", color:"#ffff",
+    padding: "20px"}}>
+        <h1 style={{fontWeight:"bold"}}>Mi primer tarea</h1>
+        <p>Tarea realizada</p>
+    </div>
+}
--- curso-react/src/index.js
+++ curso-react/src/index.js
@@ -1,38 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import { Usercard } from "./Greeting"; //Importanto nuestra funcion
-import { Button } from "./Button";
+import { TaskCard } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //Creamos un root
 
 root.render(
   <>
-  {/*De esta manera realizamos comentario*/}
-  <Button text="Click me"/>
-  <Button text="Pay"/>
-  <Button text="Log out"/>
-  <Button/>
-    {/* <Usercard
-      name="Ryan Ray"
-      amount={3000}
-      married={true}
-      points={[99, 33, 22.2]}
-      address={{street: "145 # 132", city: "Medellín" }}
-      greet={function(){alert("Hello")}}
-    />
-    <Usercard
-      name="Sauil Ray"
-      amount={4000}
-      married={false}
-      points={[99, 33, 22.2]}
-      address={{street: "125 # 132", city: "Cartagena" }}
-    />
-    <Usercard
-      name="Miguel Ray"
-      amount={2500}
-      married={true}
-      points={[910, 123, 122.2]}
-      address={{street: "115 # 132", city: "Bucaramanga" }}
-    /> */}
+    <TaskCard/>
   </>
 );
