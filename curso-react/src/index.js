fbe3cb9c337f56ad6d257c13ee6633d1aa324907
Alex <alrodriguez41@poligran.edu.co>
2022-11-09 10:58:28 -0500

    Se entiende que es un fragment

--- curso-react/src/index.js
+++ curso-react/src/index.js
@@ -6,26 +6,34 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root")); //Creamos un root
 
 function Greeting() {
-  const texto = true;
+  
+  const user = {
+    firstname: 'raul',
+    lastname: 'jimenez',
+    trabaja:true
+  }
 
-  //const name = "Raul"
-  /*
-    if(texto){
-        return <p>{name} : Esto es un texto</p>
-    }else{
-        return <p>No es un texto</p>
-    }
-    */
-  //Con un operador ternatio
-  return <h1>{texto ? "Esto es un texto 😁" : "No es un texto 😪"}</h1>;
-  /*
-    //JSX - Combinación de JS y HTML - 
-    return <h1>{name} {10 +10} {true}</h1> //Interpreta la variable name gracias a jsx 
-    //true no lo muestra
-    */
+  function add(x,y){
+    return x+y
+  }
+
+  //Convertir a String un json
+  //JSON.stringify
+  return <>
+    <h1>{add(10,30)}</h1>
+    <h1>{user.firstname}</h1>
+    <h3>{user.lastname}</h3>
+    <h3>{user.trabaja.toString()}</h3>
+  </>;
+
 }
 
+//Frangment <>  </> es una  etiqueta padre que encierra todas las etiquetas, sin necesidad de poner varios DIV
 root.render(
-  //Self closing tags para los componentes
-  <Greeting />
+  <>
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
+  </>
 );
