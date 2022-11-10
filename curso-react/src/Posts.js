8084a35e2bc25ed8fbe6c5ee5c12fade5cadeda9
Alex <alrodriguez41@poligran.edu.co>
2022-11-10 08:32:59 -0500

    Fetch y promesas

---
+++ curso-react/src/Posts.js
@@ -0,0 +1,12 @@
+//JsonPlaceHolder permite obtener datos de ejemplo
+export const Post = () =>{
+    return <button onClick={() =>{
+        //Fecth oermite traer datos
+        fetch("https://jsonplaceholder.typicodade.com/posts")
+        .then(response => response.json()) //Vas a recibir una respuesta que es la respuesta del backend a formato json
+        .then(data => console.log(data))
+        .catch(error => console.log(error)) //Si ocurre un error consumiendo la info nos muestra el error por consola
+    }}>
+        Traer datos
+    </button>
+}
