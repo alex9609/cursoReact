357b443cb0553ef93e4f1ecc6735e97f24678c1b
Alex <alrodriguez41@poligran.edu.co>
2022-11-10 14:38:56 -0500

    Usando useState

--- curso-react/src/index.js
+++ curso-react/src/index.js
@@ -7,18 +7,16 @@ const Counter = () => {
   //Este state es un elemento que tiene un array y al lado una funcion
   //counter es la variable , setCounter es la funcion que me permite modificar esa variable
   //el 0 en useState(0) es para inicializar nuestro counter a 0
-  const [counter, setCounter] = useState(10)
+  const [mensaje, setMensaje] = useState("")
 
 
 
   return <>
     <div>
-      <h1>Counter : {counter}</h1>
-      <button onClick={() => {setCounter(counter + 1)}}>sumar</button>
-      <hr></hr>
-      <button onClick={() => {setCounter(counter - 1)}}>restar</button>
-      <hr></hr>
-      <button onClick={() => {setCounter(10)}}>restart</button>
+      <input onChange={e => {setMensaje(e.target.value)}}/>
+      <button onClick={() => {
+        alert("El mensaje es: " + mensaje)
+      }}>Save</button>
     </div>
   </>
 }
