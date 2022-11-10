4907d21c74a91125870323b6739e8b231b0348d2
Alex <alrodriguez41@poligran.edu.co>
2022-11-10 11:51:01 -0500

    Manejo de arreglos con React

--- curso-react/src/index.js
+++ curso-react/src/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Post } from "./Posts";
 const root = ReactDOM.createRoot(document.getElementById("root")); //Creamos un root
 
+const user = [
+    { 
+      id:1,
+      name: 'raul ray',
+      image: 'https://robohash.org/user'
+    },
+    { 
+      id:2,
+      name: 'migue ray',
+      image: 'https://robohash.org/user2'
+    },
+    { 
+      id:3,
+      name: 'catan ray',
+      image: 'https://robohash.org/user4'
+    }
+]
 
+//Iteracion de arreglos
+//Se hace mediante metodos de arreglos
+//funcion map,filter,reduce,foreach
 root.render(
   <>
-    <Post/>
+    {user.map((user,index) =>{
+      return <div key={user.id}>
+                <h1>{user.name} {index}</h1>
+                <img src={user.image} alt=""/>
+             </div>
+    })}
   </>
 );
 
