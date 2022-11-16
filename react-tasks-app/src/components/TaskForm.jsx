813e41c75d7d7a3759f1e946a57efaa76ecae45c
Alex <alrodriguez41@poligran.edu.co>
2022-11-16 07:22:46 -0500

    Estilos con tailwindCss

--- react-tasks-app/src/components/TaskForm.jsx
+++ react-tasks-app/src/components/TaskForm.jsx
@@ -21,16 +21,23 @@ export default function TaskForm() {
 
   return (
 	<>
-	<form onSubmit={handleSubmit}>
+	<div className='max-w-md mx-auto'>
+	<form onSubmit={handleSubmit} className='bg-slate-800 p-10 mb-4'>
+		<h1 className='text-2xl font-bold mb-3 text-white'>Crea tu tarea</h1>
 		<input placeholder='Escribe tu tarea'
 		onChange={(e) => {setTitle(e.target.value)}}
 		value={title}
-		autoFocus/>
+		autoFocus
+		className='bg-slate-300 p-3 w-full mb-2'/>
 		<textarea placeholder='Escribe la descripciósn'
 		onChange={(e) => {setDescription(e.target.value)}}
-		value={description}/>
-		<button> Guardar</button>
+		value={description} 
+		className='bg-slate-300 p-3 w-full mb-2'/>
+		<button
+		className='bg-indigo-500 px-3 py-1 text-white rounded-md'
+		> Guardar</button>
 	</form>
+	</div>
 	</>
   )
 }
--- react-tasks-app/src/components/TaskList.jsx
+++ react-tasks-app/src/components/TaskList.jsx
@@ -7,11 +7,11 @@ function TaskList() {
 	const {tasks} = useContext(TaskContext)
 
 	if(tasks.length === 0){
-		return <h1>No hay tareas aún</h1>
+		return <h1 className="text-white text-4xl text-bold text-center">No hay tareas aún</h1>
 	}
 
   return (
-	<div>
+	<div className="grid grid-cols-4 gap-2">
 		{
 			tasks.map((task) =>(
 				<TaskCard key={task.id} task={task}/>
